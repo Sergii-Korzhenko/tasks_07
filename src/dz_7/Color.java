a7acf5f635e99b20c72287d958f4db6cde7df524
Sergii-Korzhenko <ventoos1@gmail.com>
2021-08-17 16:12:22 +0300

    add dz-7

--- src/dz_7/Color.java
+++ src/dz_7/Color.java
@@ -4,9 +4,9 @@ public enum Color {
 
     RED("#ff0000"),
     GREEN("#009900"),
-    BLUE("0000ff"),
-    WHITE("ffffff"),
-    BLACK("00000");
+    BLUE("#0000ff"),
+    WHITE("#ffffff"),
+    BLACK("#00000");
 
     private String hex;
 
